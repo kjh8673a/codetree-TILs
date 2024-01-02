4c879f1a0abff104733ff9d72494614a230614c8
Jihoon Gweon <kjh8673a@naver.com>
2024-01-02 10:35:22 +0900

    Title: 무~, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240102/무~/moo.java
+++ 240102/무~/moo.java
@@ -6,14 +6,40 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        String s = "moo";
-        String mid = "moo";
-        while(s.length() < N) {
-            mid += "o";
-            s = s + mid + s;
+        int len = 3;
+        int t = 0;
+        while(len < N) {
+            t++;
+            len = len * 2 + 1 + (t + 2);
         }
 
-        System.out.println(s.charAt(N - 1));
+        char c = findCharacter(N, len, t);
+        System.out.println(c);
+    }
+
+    private static Character findCharacter(int N, int len, int t) {
+        if(t == 0) {
+            if(N == 1) {
+                return 'm';
+            }else {
+                return 'o';
+            }
+        }
+
+        int mid = 1 + (t + 2);
+        int left = (N - mid) / 2;
+
+        if(N <= left) {
+            return findCharacter(N, left, t - 1);
+        }else if(N > left && N <= left + mid) {
+            if(len == left + 1) {
+                return 'm';
+            }else {
+                return 'o';
+            }
+        }else {
+            return findCharacter(N - mid + left, left, t - 1);
+        }
     }
 
 }
