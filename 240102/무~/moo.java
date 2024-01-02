6f097295dd35e51fb396997475a14f51677ae278
Jihoon Gweon <kjh8673a@naver.com>
2024-01-02 10:20:29 +0900

    Title: 무~, Time: 458ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240102/무~/moo.java
+++ 240102/무~/moo.java
@@ -1,29 +1,19 @@
 import java.io.*;
 
 public class Main {
-    static String s;
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        s = "moo";
-        int t = 1;
+        String s = "moo";
+        String mid = "moo";
         while(s.length() < N) {
-            makeMoo(t++);
+            mid += "o";
+            s = s + mid + s;
         }
 
         System.out.println(s.charAt(N - 1));
     }
 
-    private static void makeMoo(int t) {
-        String tmp = s;
-        tmp += "m";
-        for(int i = 0; i < t + 2; i++) {
-            tmp += "o";
-        }
-        tmp += s;
-
-        s = tmp;
-    }
 }
