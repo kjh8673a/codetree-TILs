b97980190b400bc1a0297fe77050605b55055dfd
Jihoon Gweon <kjh8673a@naver.com>
2024-01-02 10:51:35 +0900

    Title: 무~, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

--- 240102/무~/moo.java
+++ 240102/무~/moo.java
@@ -6,6 +6,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
+        // moo의 길이와 현재 몇단계인지 구한다.
         int len = 3;
         int t = 0;
         while(len < N) {
@@ -21,6 +22,7 @@ public class Main {
             return n == 1 ? 'm' : 'o';
         }
 
+        // 왼쪽, 중간, 오른쪽 세 부분으로 나눠 처리.
         int mid = 1 + (t + 2);
         int left = (len - mid) / 2;
 
