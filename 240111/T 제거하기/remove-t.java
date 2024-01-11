de5e71e6dc6b8ec93d8933512ad018886e7cd14f
Jihoon Gweon <kjh8673a@naver.com>
2024-01-11 11:00:44 +0900

    Title: T 제거하기, Time: 880ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240111/T 제거하기/remove-t.java
+++ 240111/T 제거하기/remove-t.java
@@ -4,13 +4,36 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String s = br.readLine();
         String t = br.readLine();
+        char tail = t.charAt(t.length() - 1);
 
-        while(s.contains(t)) {
-            s = s.replaceAll(t, "");
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            stack.add(s.charAt(i));
+            
+            if(stack.size() >= t.length() && stack.peek() == tail) {
+                String tmp = "";
+                for(int j = 0; j < t.length(); j++) {
+                    tmp = stack.pop() + tmp;
+                }
+
+                if(!tmp.equals(t)) {
+                    for(int j = 0; j < t.length(); j++) {
+                        stack.add(tmp.charAt(j));
+                    }
+                }
+            }
+
         }
 
-        System.out.println(s);
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb.reverse().toString());
     }
 }
