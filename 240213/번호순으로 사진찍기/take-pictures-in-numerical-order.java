06f7a3574de96b7b8ce5e9ee1968ed51736d16d0
Jihoon Gweon <kjh8673a@naver.com>
2024-02-13 10:29:14 +0900

    Title: 번호순으로 사진찍기, Time: 276ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240213/번호순으로 사진찍기/take-pictures-in-numerical-order.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[k][2];
+        for(int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[i][0] = Math.min(a, b);
+            arr[i][1] = Math.max(a, b);
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+           if (o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            }
+            return Integer.compare(o1[0], o2[0]); 
+        });
+
+        int answer = 0;
+        int line = 0;
+        for(int i = 0; i < k; i++) {
+            if(arr[i][0] < line) {
+                continue;
+            }else {
+                line = arr[i][1];
+                answer++;
+            }
+        }
+        if(line < n) {
+            answer++;
+        }        
+
+        System.out.println(answer);
+    }
+}
